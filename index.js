f7b9b13e23a85effc8726013e550aa337862d72e
olsen-stettsen <ols17030@byui.edu>
2019-03-24 14:28:35 -0600

    init attempt 2

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(5000, function () {
-  console.log('Example app listening on port 5000!');
+app.listen(process.env.PORT || 5000, function(){
+    console.log('Server is running on port 5000');
 });

35afb66d756a7bd1fbbd7de376834610a1b808dc
olsen-stettsen <ols17030@byui.edu>
2019-03-25 00:50:25 -0600

    init attempt 2

--- public/main.js
+++ public/main.js
@@ -2,6 +2,6 @@ function onload(){
     greet();
 }
 function greet(){
-    alert(localStorage.getItem('username'));
+    console.log("Main.js localstorage username" + localStorage.getItem('username'));
     //document.getElementById("welcometag").innerHTML = localStorage.getItem('username');
 }

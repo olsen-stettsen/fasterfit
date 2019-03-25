79dea076d983ad988d51d5c017a85953e25df75d
olsen-stettsen <ols17030@byui.edu>
2019-03-24 23:54:12 -0600

    init attempt 2

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ function posttodb(username, email, password){
   
   client.connect();
   
-  client.query('INSERT INTO account (user_name, user_email, user_password) VALUES (' + username + ',' + email + ',' + password+ ');', (err, res) => {
+  client.query('INSERT INTO account (user_name, user_email, user_password) VALUES (\'' + username + '\',\'' + email + '\',\'' + password+ '\');', (err, res) => {
     var results = [];
     if (err) throw err;
     for (let row of res.rows) {

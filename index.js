9fb63fd82a7d5f4b2efef85cc00325f618b859aa
olsen-stettsen <ols17030@byui.edu>
2019-03-24 21:44:04 -0600

    init attempt 2

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false })); 
+app.use(bodyParser.json());
 
 /*
 app.get('/', function (req, res) {

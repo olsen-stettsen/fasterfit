fcf7370fed4170d64dfd1936869766508a4c8cf8
olsen-stettsen <ols17030@byui.edu>
2019-03-24 23:49:45 -0600

    init attempt 2

--- index.js
+++ index.js
@@ -15,18 +15,19 @@ app.get("/", function(req, res){
     res.sendFile(path.join(__dirname+'/home.html'));
 });
 app.post("/update", function(req, res){
-  var name = req.body.name;
-  var excercise = req.body.excercise;
-  var weight = req.body.weight;
-  posttodb(name, excercise, weight);
+  var username = req.body.username;
+  var email = req.body.email;
+  var password = req.body.password;
+  posttodb(username, email, password);
   console.log("Request for update");
   res.sendFile(path.join(__dirname+'/public/update.html'));
 });
 
-function posttodb(name, excercise, weight){
-  console.log("name: " + name);
-  console.log("excercise: " + excercise);
-  console.log("weight: " + weight);
+function getfromdb(){
+  /*
+  console.log("user name: " + name);
+  console.log("email: " + excercise);
+  console.log("password: " + weight);*/
 
   const client = new Client({
     connectionString: process.env.DATABASE_URL,
@@ -44,6 +45,29 @@ function posttodb(name, excercise, weight){
     client.end();
     localStorage.setItem("results", JSON.stringify(results));
     console.log(JSON.stringify(results));
+  });
+}
+
+function posttodb(username, email, password){
+  console.log("user name: " + username);
+  console.log("email: " + email);
+  console.log("password: " + password);
+
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+  });
+  
+  client.connect();
+  
+  client.query('INSERT INTO account (user_name, user_email, user_password) VALUES (' + username + ',' + email + ',' + password+ ');', (err, res) => {
+    var results = [];
+    if (err) throw err;
+    for (let row of res.rows) {
+      results.push(row);
+    }
+    client.end();
+    getfromdb();
   });
 }
 

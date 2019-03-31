8c6893182ce002d51fc4490544bc27d537b525c9
olsen-stettsen <ols17030@byui.edu>
2019-03-31 01:53:37 -0600

    week12

--- public/main.js
+++ public/main.js
@@ -5,7 +5,13 @@ function greet(){
     //document.getElementById("welcometag").innerHTML = localStorage.getItem('username');
 }
 function setCalander(){
+    // set month
     var d = new Date();
     var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
     document.getElementById("header").innerHTML = months[d.getMonth()];
+    // populate cal
+    var activedate = d.getDate();
+    var tempdate = newDate();
+    tempdate.setDate(1);
+    alert(tempdate.getDate());
 }

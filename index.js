59176a3c83785f55bf055f54abfa757746b1f3b9
olsen-stettsen <ols17030@byui.edu>
2019-04-01 00:48:25 -0600

    week12

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.post("/main", function(req, res){
   }, 1000);
 });
 app.post("/writeworkout", function(req, res){
-  console.log(req);
+  console.log(JSON.stringify(req));
   postworkouttodb();
 });
 function getfromdb(){

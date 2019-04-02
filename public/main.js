a294844b06ecbc1542f4ff5158127ea4b3b3f430
olsen-stettsen <ols17030@byui.edu>
2019-04-01 18:44:17 -0600

    week12

--- public/main.js
+++ public/main.js
@@ -96,7 +96,7 @@ function postexercise(exercise){
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            //alert(this.responseText)
+            alert(this.responseText)
         }
     };
     xmlhttp.open("POST", "/writeworkout", true);

72dbab7a1ca65cb8028c763a061a0e2b09923007
olsen-stettsen <ols17030@byui.edu>
2019-04-01 18:42:47 -0600

    week12

--- public/main.js
+++ public/main.js
@@ -93,23 +93,16 @@ function enterexercise(){
     postexercise(JSON.stringify(exercise));
 }
 function postexercise(exercise){
-    $.ajax({
-        global: false,
-        type: 'POST',
-        url: '/writeworkout',
-        dataType: 'html',
-        data: {
-            name: $("#profile_name").val(),
-            surname: $("#profile_surname").val(),
-            age: $("#profile_age").val()
-        },
-        success: function (result) {
-            console.log(result);
-        },
-        error: function (request, status, error) {
-            serviceError();
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            //alert(this.responseText)
         }
-    });
+    };
+    xmlhttp.open("POST", "/writeworkout", true);
+    xmlhttp.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+    xmlhttp.send(exercise);
+
 }
 function getdbfromhtml(){
     //alert(document.getElementById("workoutdata").innerHTML);

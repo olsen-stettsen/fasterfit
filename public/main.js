76b1663150178e847277084857e164b956c607b7
olsen-stettsen <ols17030@byui.edu>
2019-04-05 14:45:01 -0600

    week13

--- public/main.js
+++ public/main.js
@@ -27,7 +27,8 @@ function setCalander(date){
         }
         var cell = row.insertCell(); 
         if (workoutdataonthisday(dPos, date.getMonth())){
-            cell.style.background = "green";
+            //cell.style.background = "green";
+            alert("found one");
         }
         
         if(dPos > firstday){

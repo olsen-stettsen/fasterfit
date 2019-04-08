e5d68a966062a4220d745fd585b39d6647d45cdb
olsen-stettsen <ols17030@byui.edu>
2019-04-07 22:41:09 -0600

    week13

--- index.js
+++ index.js
@@ -147,7 +147,7 @@ function make_recs() {
   }
   var recomendationsforyousir = "";
   for(var s = 0; s < mostrecent.length; s++){
-    var workyout = mostrecent[s];
+    var workyout = mostrecent[s].sets;
     console.log("ssssss: " + workyout);
   }
   return JSON.stringify(mostrecent);

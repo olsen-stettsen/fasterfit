b8b9ecb4adac82e7670b7ffa0e638dde261e0e39
olsen-stettsen <ols17030@byui.edu>
2019-04-08 00:55:35 -0600

    week13

--- public/main.js
+++ public/main.js
@@ -59,7 +59,7 @@ function getMonthstring(index){
     return months[index];
 }
 function setrecs(){
-    document.getElementById("datadisplay3").innerHTML = document.getElementById("datadisplay3").innerHTML.replace("\"","").replace("\"","").replace("&lt;", "<").replace("&gt;", ">");
+    document.getElementById("datadisplay3").innerHTML = document.getElementById("datadisplay3").innerHTML.replace("&lt;", "<").replace("&gt;", ">");
     /*
     var content = document.getElementById("datadisplay3").innerHTML.replace("\"","").replace("\"","");
     var woko = content.split("p1");

1ea04b44ffe8be6c43e60820be1b43120f76cc3f
olsen-stettsen <ols17030@byui.edu>
2019-04-08 21:52:59 -0600

    week13

--- public/startdb.sql
+++ public/startdb.sql
@@ -9,3 +9,5 @@ CREATE TABLE exercise (
     exercise_name   varchar(90)     NOT NULL,
     sets_reps_json  varchar(250)    NOT NULL
 );
+INSERT INTO account VALUES ('testuser', 'testuser@tu.com', '1234');
+INSERT INTO exercise VALUES ((SELECT user_id FROM account WHERE user_name='testuser'), 'Bench', '{"sets":["255,12","315,8","335,6","355,3"],"name":"Bench","year":2019,"month":3,"day":7,"hour":10,"minutes":59}');
